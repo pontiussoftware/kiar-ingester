d8f0d684e90ecc613dae18cb07c28a56dfea2260
Ralph Gasser <rg@pontius.ch>
2022-10-15 11:59:10 +0200

    Bugfix!

--- src/main/kotlin/ch/pontius/ingester/parsing/xml/XmlParsingContext.kt
+++ src/main/kotlin/ch/pontius/ingester/parsing/xml/XmlParsingContext.kt
@@ -2,7 +2,6 @@ package ch.pontius.ingester.parsing.xml
 
 import ch.pontius.ingester.config.MappingConfig
 import ch.pontius.ingester.parsing.values.ValueParser
-import ch.pontius.ingester.processors.sources.XmlFileSource
 import org.apache.solr.common.SolrInputDocument
 import org.xml.sax.Attributes
 import org.xml.sax.helpers.DefaultHandler
@@ -19,13 +18,13 @@ import java.util.*
 class XmlParsingContext(config: MappingConfig, val callback: (SolrInputDocument) -> Unit): DefaultHandler() {
 
     /** The current [SolrInputDocument] that is being processed. */
-    private var document = SolrInputDocument()
-
-    /** The current XPath this [XmlParsingContext] is currently in. */
-    private val stack = Stack<String>()
+    private val document = SolrInputDocument()
 
     /** The current XPath this [XmlParsingContext] is currently in. */
     private var xpath = "/"
+
+    /** The current XPath this [XmlParsingContext] is currently in. */
+    private val stack = Stack<String>()
 
     /** The current XPath this [XmlParsingContext] is currently in. */
     private val parsers = HashMap<XmlAttributeMapping, ValueParser<*>>()
@@ -73,11 +72,13 @@ class XmlParsingContext(config: MappingConfig, val callback: (SolrInputDocument)
      * Removes the new element from the stack
      */
     override fun endElement(uri: String, localName: String, qName: String) {
+        val previousMappings = this.mappings[this.xpath]
+
+        /* Pop stack. */
         this.stack.pop()
         this.xpath = "/${this.stack.joinToString("/")}"
 
         /* Flush old context into document (if required). */
-        val previousMappings = this.mappings[this.xpath]
         if (previousMappings != null) {
             for (m in previousMappings) {
                 val value = this.parsers[m]?.get()
@@ -86,11 +87,12 @@ class XmlParsingContext(config: MappingConfig, val callback: (SolrInputDocument)
                 }
             }
         }
+        this.parsers.clear()
 
         /* Flush old document (if needed). */
         if (this.xpath == this.newDocumentOn) {
-            this.callback(this.document)
-            this.document = SolrInputDocument()
+            this.callback(this.document.deepCopy())
+            this.document.clear()
         }
     }
 
