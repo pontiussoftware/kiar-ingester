13a50be8937a9bc40745ff9fb6aaca9b93249c10
Ralph Gasser <rg@pontius.ch>
2023-04-27 16:53:09 +0200

    Fixed SLF4j integration.

--- kiar-ingest/build.gradle.kts
+++ kiar-ingest/build.gradle.kts
@@ -8,6 +8,7 @@ val kotlinCoroutines: String by project
 val kotlinSerializaion: String by project
 val log4jVersion: String by project
 val solrjVersion: String by project
+val slf4jVersion: String by project
 val twelveMonkeysVersion: String by project
 val xodusVersion: String by project
 val xodusDnqVersion: String by project
@@ -37,7 +38,8 @@ dependencies {
     /** Log4j2 & SLF4j */
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4jVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
 
     /** Kotlinx. */
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinSerializaion")
