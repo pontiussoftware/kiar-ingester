d030e6c4ac9467fa9a8b57ad3df120985d157541
Ralph Gasser <rg@pontius.ch>
2023-07-11 17:54:41 +0200

    Added missing navigation.

--- kiar-ui/src/app/components/dashboard/logs/job-log.component.ts
+++ kiar-ui/src/app/components/dashboard/logs/job-log.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
 import {JobService} from "../../../../../openapi";
 import {JobLogDatasource} from "./job-log-datasource";
-import {tap} from "rxjs";
+import {asapScheduler, map, Observable, tap} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {MatPaginator} from "@angular/material/paginator";
 
@@ -18,10 +18,14 @@ export class JobLogComponent implements AfterViewInit, OnInit {
   /** The columns displayed in the data table. */
   public readonly displayedColumns= ["documentId", "level", "context", "description"];
 
+  public readonly jobId: Observable<string>
+
   /** Reference to the {@link MatPaginator}*/
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private service: JobService, private route: ActivatedRoute) {}
+  constructor(private service: JobService, private route: ActivatedRoute) {
+    this.jobId = this.route.paramMap.pipe(map(p => p.get('id')!!))
+  }
 
   /**
    * Initializes the data source and load the data.
@@ -37,4 +41,6 @@ export class JobLogComponent implements AfterViewInit, OnInit {
   public ngAfterViewInit() {
     this.paginator.page.pipe(tap(() => this.dataSource.load(this.paginator.pageIndex, this.paginator.pageSize))).subscribe();
   }
+
+  protected readonly asapScheduler = asapScheduler;
 }
