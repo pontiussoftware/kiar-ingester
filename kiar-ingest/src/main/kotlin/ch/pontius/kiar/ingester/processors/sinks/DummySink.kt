52024699de654b49610f948e0e9a150b1790cd68
Ralph Gasser <rg@pontius.ch>
2023-07-14 17:18:07 +0200

    Added DummySink for testing.

---
+++ kiar-ingest/src/main/kotlin/ch/pontius/kiar/ingester/processors/sinks/DummySink.kt
@@ -0,0 +1,26 @@
+package ch.pontius.kiar.ingester.processors.sinks
+
+import ch.pontius.kiar.ingester.processors.ProcessingContext
+import ch.pontius.kiar.ingester.processors.sources.Source
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.apache.solr.common.SolrInputDocument
+
+/**
+ * A dummy [Sink] implementation used for debugging.
+ *
+ * @author Ralph Gasser
+ * @version 1.0.0
+ */
+class DummySink(override val input: Source<SolrInputDocument>): Sink<SolrInputDocument> {
+    override fun toFlow(context: ProcessingContext): Flow<Unit> {
+        return flow {
+            /* Start collect incoming flow. */
+            this@DummySink.input.toFlow(context).collect() {
+            }
+
+            /* Finalize. */
+            emit(Unit)
+        }
+    }
+}
