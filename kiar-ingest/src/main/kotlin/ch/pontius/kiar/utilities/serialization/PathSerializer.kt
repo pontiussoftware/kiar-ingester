9efa13c733a7e2977dc51f62ab2ba2fedbe19253
Ralph Gasser <rg@pontius.ch>
2024-08-07 15:52:16 +0200

    Removes superfluous annotation.

--- kiar-ingest/src/main/kotlin/ch/pontius/kiar/utilities/serialization/PathSerializer.kt
+++ kiar-ingest/src/main/kotlin/ch/pontius/kiar/utilities/serialization/PathSerializer.kt
@@ -1,8 +1,6 @@
 package ch.pontius.kiar.utilities.serialization
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -17,8 +15,6 @@ import java.nio.file.Paths
  * @author Ralph Gasser
  * @version 1.0.0
  */
-@ExperimentalSerializationApi
-@Serializer(forClass = Path::class)
 object PathSerializer : KSerializer<Path> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("NIOPathSerializer", PrimitiveKind.STRING)
     override fun deserialize(decoder: Decoder): Path = Paths.get(decoder.decodeString())
