113a6d438368fa02be809e39abaa5630823f0e6c
Ralph Gasser <rg@pontius.ch>
2025-06-19 14:41:15 +0200

    Updates NPM version.

--- kiar-ui/build.gradle.kts
+++ kiar-ui/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.kotlin.dsl.*
 
 plugins {
-    id("com.github.node-gradle.node") version "7.0.2"
+    id("com.github.node-gradle.node") version "7.1.0"
 }
 
 configurations {
@@ -14,7 +14,7 @@ configurations {
 
 val includeConfig: Boolean by lazy { project.hasProperty("includeConfig") }
 node {
-    this.version.value("20.16.0")
+    this.version.value("22.16.0")
     this.download.value(true)
     this.workDir.dir("${project.projectDir}/.gradle/nodejs")
     this.yarnWorkDir.dir("${project.projectDir}/.gradle/nodejs")
