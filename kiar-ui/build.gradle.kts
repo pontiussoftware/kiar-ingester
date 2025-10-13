e2617459ef9a0d9ea7536624942dc6b742f0202e
Ralph Gasser <rg@pontius.ch>
2025-10-13 08:14:07 +0200

    Updates Node.js version

--- kiar-ui/build.gradle.kts
+++ kiar-ui/build.gradle.kts
@@ -14,7 +14,7 @@ configurations {
 
 val includeConfig: Boolean by lazy { project.hasProperty("includeConfig") }
 node {
-    this.version.value("22.17.0")
+    this.version.value("22.20.0")
     this.download.value(true)
     this.workDir.dir("${project.projectDir}/.gradle/nodejs")
     this.yarnWorkDir.dir("${project.projectDir}/.gradle/nodejs")
