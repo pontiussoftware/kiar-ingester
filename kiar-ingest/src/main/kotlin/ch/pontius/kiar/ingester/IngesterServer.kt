6718b7d3b3c9a64a5d1e54fba7de3f003892164d
Ralph Gasser <rg@pontius.ch>
2025-10-17 19:59:31 +0200

    Scheduled jobs are now also set to interrupted when server starts.

--- kiar-ingest/src/main/kotlin/ch/pontius/kiar/ingester/IngesterServer.kt
+++ kiar-ingest/src/main/kotlin/ch/pontius/kiar/ingester/IngesterServer.kt
@@ -18,6 +18,7 @@ import kotlinx.coroutines.flow.takeWhile
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.core.or
 import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.jetbrains.exposed.v1.jdbc.update
@@ -89,7 +90,7 @@ class IngesterServer(val config: Config) {
             }
 
             /* Mark jobs that are still running as interrupted. */
-            Jobs.update({ Jobs.status eq JobStatus.RUNNING }) { update ->
+            Jobs.update({ (Jobs.status eq JobStatus.RUNNING) or (Jobs.status eq JobStatus.SCHEDULED)}) { update ->
                 update[status] = JobStatus.INTERRUPTED
                 update[modified] = Instant.now()
             }
