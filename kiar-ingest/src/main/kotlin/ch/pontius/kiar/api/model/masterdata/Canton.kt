aff22d8c5edf412afc2ba1c153ee38acde2829fa
Ralph Gasser <ralph.gasser@unibas.ch>
2025-11-19 09:38:54 +0100

    Minor change to enumeration.

--- kiar-ingest/src/main/kotlin/ch/pontius/kiar/api/model/masterdata/Canton.kt
+++ kiar-ingest/src/main/kotlin/ch/pontius/kiar/api/model/masterdata/Canton.kt
@@ -1,14 +1,11 @@
 package ch.pontius.kiar.api.model.masterdata
 
-import kotlinx.serialization.Serializable
-
 /**
  * A representation of a [Canton].
  *
  * @author Ralph Gasser
  * @version 1.1.0
  */
-@Serializable
 enum class Canton(val longName: String) {
     AG("Aargau"),
     BE("Bern"),
