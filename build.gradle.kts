264081d5c228fde77711539d55645b8c32cfb220
Ralph Gasser <rg@pontius.ch>
2026-09-21 19:19:02 +0200

    Version increment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ subprojects {
     group = "ch.pontius.kiar"
 
     /* Our current version, on dev branch this should always be release+1-SNAPSHOT */
-    version = "1.5.0"
+    version = "1.6.0"
 
     tasks {
         compileKotlin {
